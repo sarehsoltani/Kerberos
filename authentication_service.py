9a9ce4f526c574a4bc2fc509e6b8756d4c6cc1f1
SarehSoltani <sare.soltani74@gmail.com>
2021-11-05 16:06:13 -0400

    Initial setup

---
+++ authentication_service.py
@@ -0,0 +1,58 @@
+"""
+ Contains the class representing an authentication server to authenticate
+ users through the kerberos protocol.
+ @author Sajal Saha & Kirk Vander Ploeg
+"""
+import shared_secret_key
+import dataset
+import random
+import ticket_granting_service
+
+class AS:
+    def __init__(self):
+        self.SSK = shared_secret_key.SHARED_KEY_BTN_USER_AS  #Secret key shared between user and authentication server
+        self.TGS = ticket_granting_service.TGS()  #Ticket granting service object, used to access the TGT list
+    """
+    This method authenticates the encrypted user name and returns a TGT to 
+    the user. Use the following steps to complete this method:
+        1) decrypt the encrypted username
+        2) if the decrypted username is valid (in the dataset):
+            a) generate a random index between 0-4 and use it to retrieve
+            a Ticket Granting Ticket (TGT) from the list in the TGS object 
+            (self.TGS). 
+            b) return the tgt
+        3) If the decrypted username was not valid, return None. For this
+        task, assume that the TGTs are already encrypted. IE. you can return 
+        the exact element from the list.
+    
+    @param encryptedUserName encrypted user name
+    @return tgt if authentication successful otherwise return 0
+    """
+    def authenticate(self, encryptedUserName):
+        """
+        Implementation Here. Replace None with correct statements on each line. 
+        """
+        decryptedUserName = None    # Decrypt the passed encrypted username
+        if None:                    # Update the condition in the if statement.You need to check whether our Dataset's USER_LIST contains the decryptedUserName or not. 
+            index = None            # Generate random index fulfilling the above condition. 
+            tgt = None              # Use the index to access corresponding TGT from TGS. 
+            return tgt              # return tgt
+        else:
+            return None             # user not valid, return None
+    
+
+    """
+    This function decrypts the user information (username). 
+    To do this, we must undo the encryption that was done in user.py and is
+    left for you to figure out!
+    @param value user information to be decrypted
+    @return decrypted user name
+    """
+    def decryptUserInfo(self, value):
+        """
+        Implementation Here. Read the provided document
+        carefully before implementing. 
+        """
+        decryptedUserInfo = ""
+
+        return decryptedUserInfo  #return the decryptedUserInfo
