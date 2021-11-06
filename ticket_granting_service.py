aa581769c9b81820011fb9deea7e00afdead3de9
SarehSoltani <sare.soltani74@gmail.com>
2021-11-06 11:15:32 -0400

    encryption

--- ticket_granting_service.py
+++ ticket_granting_service.py
@@ -5,14 +5,15 @@
 import random
 import shared_secret_key
 
+
 class TGS:
     """
     Initialize the TGT and TOKEN lists
     """
 
     def __init__(self):
-        self.TGT = [12121, 123312, 87542, 5689898,124546]       #VALID TGTs
-        self.TOKEN = [14586, 41258, 14589, 54852, 54863]        #VALID TOKENS
+        self.TGT = [12121, 123312, 87542, 5689898, 124546]  # VALID TGTs
+        self.TOKEN = [14586, 41258, 14589, 54852, 54863]  # VALID TOKENS
         self.ShareKey = shared_secret_key.SHARED_KEY_BTN_TGS_FS
 
     """
@@ -21,19 +22,21 @@ class TGS:
     @param tgt TGT for the verification
     @return encrypted after verification of the successful TGT
     """
+
     def authenticateTGT(self, tgt):
         """
         Implementation Here. Replace None with correct statements on each 
         line.        
         """
         if tgt in self.TGT:  # Update if statement to check weather tgt in TGT list or not.
-            index = random.randrange(0, 4)         # create a random index
-            token = self.TOKEN[index]              # use index to retrieve a TOKEN
-            encryptedToken =  None  #Call proper method method to encrypt the token. 
-            return encryptedToken   #return the encrypted token.
+            index = random.randrange(0, 4)  # create a random index
+            token = self.TOKEN[index]  # use index to retrieve a TOKEN
+            encryptedToken = self.encryptTokenToByte(token)  # Call proper method method to encrypt the token.
+            print("token byte", encryptedToken)
+            return encryptedToken  # return the encrypted token.
         else:
-            return None             #TGT not valid, return None
-    
+            return None  # TGT not valid, return None
+
     """
     This method encrypts a token into bytes. The encryption is done by adding
     the key shared with the fileserver to the token and then converting to 
@@ -42,12 +45,11 @@ class TGS:
     @param token token from TGS
     @return byte array of the encrypted token
     """
-    def encryptTokenToByte(self,token):
-        """
-        Implementation Here. Replace None with correct statements on each line.
-        Read the provided document carefully before implementing. 
-        """
+
+    def encryptTokenToByte(self, token):
         changedToken = token + self.ShareKey
-        encryptedToken = changedToken.to_bytes((changedToken.bit_length() + 7) // 8, 'big')   #encrypt the token here
-        x.to_bytes((x.bit_length() + 7) // 8, 'big')
-        return encryptedToken #return encrypted token
+        encryptedToken = changedToken.to_bytes((changedToken.bit_length() + 7) // 8, 'big')  # encrypt the token here
+        return encryptedToken
+
+
+k = TGS().authenticateTGT(12121)
